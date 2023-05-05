43a2f166739b73f7f4dabd6338140bd684b4ad08
Anthony Hua <111655793+HelpInee@users.noreply.github.com>
2023-05-05 03:46:06 +0000

    Thu, May 4, 2023, 8:46 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 function load() {
     document.getElementById("btn").innerHTML = "Loading";
 }
+
+function load2() {
+    document.getElementById("btn2").innerHTML = "Loading";
+}
+
+function load3() {
+    document.getElementById("btn3").innerHTML = "Loading";
+}
