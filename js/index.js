4f8572ea415a0f9010ecd48d6f8fa7b80bda93a4
Anthony Hua <111655793+HelpInee@users.noreply.github.com>
2023-05-05 03:41:43 +0000

    Thu, May 4, 2023, 8:41 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
 function thing() {
-    document.getElementById("btn").style.fontSize = "25px";
     document.getElementById("btn").style.color = "red";
-    document.getElementById("btn").style.backgroundColor = "yellow";
 }
