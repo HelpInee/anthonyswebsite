496420ca0bf37acd97b1968facf8835cd063a359
Anthony Hua <111655793+HelpInee@users.noreply.github.com>
2023-05-05 03:37:57 +0000

    Thu, May 4, 2023, 8:37 PM -07:00

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function thing() {
+    document.getElementById("btn").style.fontSize = "25px";
+    document.getElementById("btn").style.color = "red";
+    document.getElementById("btn").style.backgroundColor = "yellow";
+}
