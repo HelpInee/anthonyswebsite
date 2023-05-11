39d520988b91929b2149ac174d97c6616bb68ef5
Anthony Hua <111655793+HelpInee@users.noreply.github.com>
2023-05-11 03:35:16 +0000

    Wed, May 10, 2023, 8:35 PM -07:00

--- js/colors.js
+++ js/colors.js
@@ -2,7 +2,7 @@ const button = document.querySelector('button');
 let isPink = true;
 
 button.addEventListener('click', function() {
-    const colors = ['#ff9999', '#99ff99', '#9999ff', '#ffff99', '#ff99ff', '#99ffff', '#FFA500'];
-    randomIndex = Math.floor(Math.random() * 7);
+    const colors = ['#ff9999', '#99ff99', '#9999ff', '#ffff99', '#ff99ff', '#99ffff', '#FFA500', '#009e5f'];
+    randomIndex = Math.floor(Math.random() * 8);
     document.body.style.backgroundColor = colors[randomIndex];
 });
---
+++ js/quotes.js
@@ -0,0 +1,21 @@
+const quotes = [
+    "The best way to predict the future is to create it.",
+    "Lord Chamberlain's readers or controllers, which were a handful of people working directly to him, were a very assorted group of people and some of them tried very hard to be as liberal as they could.",
+    "It does not matter how slowly you go as long as you do not stop.",
+    "I love you, hugs & kisses, smoochies galore, licks, nibbles & assorted gropages!! -Aisling said to Drake",
+    "Otis was inspired by a boy who sat across the aisle from me in sixth grade. He was a lively person. My best friend appears in assorted books in various disguises.",
+    "I won’t get upset at you about a mistake. I’ll get upset at you for the next mistake that comes from still thinking about the last mistake.",
+    "Part of being a champ is acting like a champ. You have to learn how to win and not run away when you lose.",
+    "Champions keep playing until they get it right. Then they play more.",
+    "Strive not to be a success, but rather to be of value."
+  ];
+
+  const quoteEl = document.getElementById("quote");
+  const btnEl = document.getElementById("btn");
+
+  btnEl.addEventListener("click", generateQuote);
+
+  function generateQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    quoteEl.innerHTML = quotes[randomIndex];
+  }
