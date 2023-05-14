bd24d1d848da49eec83fe0966506c5b953d871c1
Anthony Hua <111655793+HelpInee@users.noreply.github.com>
2023-05-13 19:32:33 -0700

    “apcsp”

--- js/colors.js
+++ js/colors.js
@@ -1,5 +1,4 @@
 const button = document.querySelector('button');
-let isPink = true;
 
 button.addEventListener('click', function() {
     const colors = ['#ff9999', '#99ff99', '#9999ff', '#ffff99', '#ff99ff', '#99ffff', '#FFA500', '#009e5f', '#a6c72e'];
--- js/quotes.js
+++ js/quotes.js
@@ -2,7 +2,6 @@ const quotes = [
     "The best way to predict the future is to create it.",
     "Lord Chamberlain's readers or controllers, which were a handful of people working directly to him, were a very assorted group of people and some of them tried very hard to be as liberal as they could.",
     "It does not matter how slowly you go as long as you do not stop.",
-    "I love you, hugs & kisses, smoochies galore, licks, nibbles & assorted gropages!! -Aisling said to Drake",
     "Otis was inspired by a boy who sat across the aisle from me in sixth grade.",
     "I won’t get upset at you about a mistake. I’ll get upset at you for the next mistake that comes from still thinking about the last mistake.",
     "Part of being a champ is acting like a champ. You have to learn how to win and not run away when you lose.",
@@ -10,7 +9,8 @@ const quotes = [
     "Strive not to be a success, but rather to be of value.",
     "The way to get started is to quit talking and begin doing.",
     "Whatever you are, be a good one.",
-    "Positive anything is better than negative nothing."
+    "Positive anything is better than negative nothing.",
+    "You can get everything in life you want if you will just help enough other people get what they want."
   ];
 
   const quoteEl = document.getElementById("quote");
